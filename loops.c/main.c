c22ed6d53cb5e4c08695d4372a4ca5426818e187
Masinde Brian <brianmasi3403@gmail.com>
2023-10-08 09:36:45 +0300

    added an if statement code

---
+++ loops.c/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+  int number;
+  printf("enter your value: ");
+  scanf("%d" , &number);
+(number < 18)? printf("you cannot vote"):printf("you  can  vote ");
+
+}
+
